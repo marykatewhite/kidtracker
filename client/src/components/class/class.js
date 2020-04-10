4ad32e592fa11db43fd82059caf36b1858f13091
zsadov1 <zsadov1@gmail.com>
2020-04-10 13:51:20 -0400

    Got rid of Out of the box React stuff on App.js

--- client/src/components/class/class.js
+++ client/src/components/class/class.js
@@ -2,8 +2,47 @@ import React from "react";
 
 const styles = {};
 
-function Class() {
-    return ();
+function ClassList() {
+	return (
+		<ul class='collection with-header'>
+			<li class='collection-header'>
+				<h4>Teacher's Name</h4>
+				<h4>Teacher's</h4>
+			</li>
+			<li class='collection-item'>
+				<div>
+					Alvin
+					<a href='#!' class='secondary-content'>
+						<i class='material-icons'>more_horiz</i>
+					</a>
+				</div>
+			</li>
+			<li class='collection-item'>
+				<div>
+					Alvin
+					<a href='#!' class='secondary-content'>
+						<i class='material-icons'>more_horiz</i>
+					</a>
+				</div>
+			</li>
+			<li class='collection-item'>
+				<div>
+					Alvin
+					<a href='#!' class='secondary-content'>
+						<i class='material-icons'>more_horiz</i>
+					</a>
+				</div>
+			</li>
+			<li class='collection-item'>
+				<div>
+					Alvin
+					<a href='#!' class='secondary-content'>
+						<i class='material-icons'>more_horiz</i>
+					</a>
+				</div>
+			</li>
+		</ul>
+	);
 }
 
-export default Class;
+export default ClassList;
