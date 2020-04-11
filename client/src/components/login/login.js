f31d41c9571dbbe8ce282b68a86e3997d276c693
MaryEtak <marykatewhite@gmail.com>
2020-04-11 11:38:59 -0400

    fix

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,35 +1,35 @@
 import React from "react";
 
 function Login() {
-	return (
-		<div className='row'>
-			<form className='col s12 offset-s3'>
-				<div className='row'>
-					<div className='input-field col s6'>
-						<input id='email' type='email' className='validate' />
-						<label for='email'>Email</label>
-					</div>
-					<div className='row'>
-						<div className='input-field col s6'>
-							<input id='password' type='password' className='validate' />
-							<label for='password'>Password</label>
-						</div>
-						<div className='row'>
-							<div className='col s6'>
-								<button
-									className='btn waves-effect waves-light'
-									type='submit'
-									name='action'
-								>
-									Submit
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</form>
-		</div>
-	);
+  return (
+    <div className="row">
+      <form className="col s12 offset-s3">
+        <div className="row">
+          <div className="input-field col s6">
+            <input id="email" type="email" className="validate" />
+            <label for="email">Email</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s6">
+            <input id="password" type="password" className="validate" />
+            <label for="password">Password</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s6">
+            <button
+              className="btn waves-effect waves-light"
+              type="submit"
+              name="action"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
