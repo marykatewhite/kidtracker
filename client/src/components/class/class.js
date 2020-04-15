996823580b65df0c5bd6fe9a0e26238ab5d30dd1
zsadov1 <zsadov1@gmail.com>
2020-04-15 16:30:52 -0400

    Changes to Class Comp.

--- client/src/components/class/class.js
+++ client/src/components/class/class.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./class.css";
 
-function ClassList({ teacherName }) {
+function classNameList({ teacherName }) {
 	return (
 		<>
 			<div className='col s12 m7' id='classComponent'>
@@ -12,10 +12,22 @@ function ClassList({ teacherName }) {
 							{teacherName}'s class
 						</h4>
 					</div>
+					<div className='col s2'>
+						<h4>
+							<a
+								href='#'
+								data-target='slide-out'
+								className='sidenav-trigger btn-floating btn-large waves-effect waves-dark btn grey'
+							>
+								<i className='material-icons left'>chat</i>Send
+							</a>
+						</h4>
+					</div>
 				</div>
 			</div>
+			;
 		</>
 	);
 }
 
-export default ClassList;
+export default classNameList;
