d84efc904167fa8d25be87457fcb3e6f6e29aa6a
zsadov1 <zsadov1@gmail.com>
2020-04-16 19:56:38 -0400

    Logo fix

--- client/src/pages/loginpage/login.js
+++ client/src/pages/loginpage/login.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Login from "../../components/login/login";
-
-import "./login.css";
+import Logo from "../../components/logo/logo";
 
 function LoginPage() {
 	return (
 		<>
 			<div className='row'>
 				<div className='col s12'>
-					<Login />
+					<Logo />
+					<div className='row'>
+						<div className='col s12'>
+							<Login />
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
