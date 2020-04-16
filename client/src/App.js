526b835b4472d7763eab1cc7b34eecbb2903aabf
JGSims <josi3006@hotmail.com>
2020-04-16 14:45:27 -0400

    CHat Box renders on page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Logo from "./components/logo/logo";
 import ClassList from "./components/class/class";
 import StudentProfile from "./components/studentprofile/studentProfile";
 import Login from "./components/login/login";
+import Chat from "./components/chat/chat";
 
 import "./App.css";
 import "materialize-css/dist/css/materialize.min.css";
@@ -15,9 +16,10 @@ class App extends Component {
 				{" "}
 				{/* <div>{<Navbar />}</div> */}
 				<div>{<Logo />}</div>
-				<div>{<ClassList teacherName='Mr.Simm' />}</div>
+				<div>{<ClassList teacherName='Mr.Sims' />}</div>
 				<div>{<StudentProfile />}</div>
 				<div>{<Login />}</div>{" "}
+				<div><Chat /></div>
 			</>
 		);
 	}
