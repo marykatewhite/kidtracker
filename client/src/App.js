b228da8a015534cb8aab4f2d661b37ca834549ae
zsadov1 <zsadov1@gmail.com>
2020-04-17 20:17:03 -0400

    Laid out routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
-import Logo from "./components/logo/logo";
-// import Navbar from "./components/navbar/navbar";
-import ClassList from "./components/class/class";
-import StudentProfile from "./components/studentprofile/studentProfile";
-import Login from "./components/login/login";
-import Chat from "./components/chat/chat";
+import DashboardPage from "../src/pages/dashboard/dashboard";
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import LoginPage from "./pages/loginpage/login";
@@ -16,19 +12,11 @@ class App extends Component {
 	render() {
 		return (
 			<>
-				{" "}
-				{/* <div>{<Navbar />}</div> */}
-				<div>{<Logo />}</div>
-				<div>{<ClassList teacherName='Mr.Sims' />}</div>
-				<div>{<StudentProfile />}</div>
-				<div>{<Login />}</div>{" "}
-				<div><Chat /></div>
 				<Router>
 					<div>
 						<div className='contentdiv'>
 							<Route exact path='/' component={LoginPage} />
-							{/* <Route exact path='/projects' component={Projects} />
-						<Route exact path='/resume' component={Resume} /> */}
+							<Route exact path='/dashboard' component={DashboardPage} />
 						</div>
 					</div>
 				</Router>
