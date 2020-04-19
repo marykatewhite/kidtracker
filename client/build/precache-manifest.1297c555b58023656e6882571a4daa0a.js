42c156a20d6e713b1bfe5c7a440ce3823f5c4622
MaryEtak <marykatewhite@gmail.com>
2020-04-19 10:35:31 -0400

    oh god this branch is so messed up now

---
+++ client/build/precache-manifest.1297c555b58023656e6882571a4daa0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "76198586de2dc2aad349",
+    "url": "/static/css/main.65df4d22.chunk.css"
+  },
+  {
+    "revision": "76198586de2dc2aad349",
+    "url": "/static/js/main.180ea053.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bcca25c60ec15eca0e82",
+    "url": "/static/css/2.a46fc1c0.chunk.css"
+  },
+  {
+    "revision": "bcca25c60ec15eca0e82",
+    "url": "/static/js/2.f32d02d9.chunk.js"
+  },
+  {
+    "revision": "a1b7d6bdaf0814df89b64462d8541adb",
+    "url": "/index.html"
+  }
+];
