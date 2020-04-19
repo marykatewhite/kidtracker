55b331598c623f3e3550d21ebc2816cd328dbdb1
JGSims <josi3006@hotmail.com>
2020-04-18 23:44:39 -0400

    CHat function working

--- client/src/components/chat/chat.js
+++ client/src/components/chat/chat.js
@@ -4,9 +4,6 @@ import React from "react";
 
 function Chat() {
 
-
-
-
         return (
 
             <div>
@@ -21,31 +18,7 @@ function Chat() {
                     <button>Send</button>
                 </form>
 
-
-
-                {/* <div className="chat-container">
-
-                <header className="chat-header">Chat Box</header>
-                <form>
-                    <button id="teacher-dropdown">To...</button>
-                    {/* // Insert dropdown here with teacher list */}
-
-                {/* <input
-                        id="msg"
-                        type="text"
-                        placeholder="Enter Message"
-                        required
-                    />
-                    <button id="send-button">Send</button>
-
-                    <div id="chat-messages"></div>
-
-                </form>
-            </div > */}
-
             </div>
-
-
         );
 
     }
