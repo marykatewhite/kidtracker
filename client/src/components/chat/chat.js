aeb44363c9eb7934e2febda5de7bbcde670cb6d2
zsadov1 <zsadov1@gmail.com>
2020-04-20 14:23:17 -0400

    Chat style/ Student list style

--- client/src/components/chat/chat.js
+++ client/src/components/chat/chat.js
@@ -5,39 +5,32 @@ function Chat({ teacherName }) {
 	return (
 		<div className='chattydiv'>
 			<div className='row'>
-				<div className='col s4'>
-					<div className='card transparent z-depth-0'>
-						<div class='card-content white-text'>
-							<span class='card-title' id='userName'>
-								{teacherName}
-							</span>
+				<div className='card transparent z-depth-0'>
+					<div class='card-content white-text'>
+						<span class='card-title' id='userName'>
+							{teacherName}
+						</span>
 
-							<div className='col s8'>
-								<div className='card transparent z-depth-0'>
-									<div class='card-content white-text'>
-										<span class='card-title'>Messages</span>
+						<div className='card transparent z-depth-0'>
+							<div class='card-content white-text'>
+								<span class='card-title'>Messages</span>
 
-										<ul id='messages' className='messages'></ul>
-									</div>
-								</div>
+								<ul id='messages' className='messages'></ul>
 							</div>
-
-							<form action=''>
-								<span id='nm'>testuser></span>
-								<input
-									id='m'
-									type='text'
-									placeholder='Enter message here'
-									autocomplete='off'
-									className='input-field green-text'
-								/>
-								<button
-									type='submit'
-									className='btn waves-effect waves-light grey'>
-									Send
-								</button>
-							</form>
 						</div>
+						<form action=''>
+							<span id='nm'>Mr.Smith's class</span>
+							<input
+								id='m'
+								type='text'
+								placeholder='Enter message here'
+								autocomplete='off'
+								className='input-field green-text'
+							/>
+							<button type='submit' className='btn waves-effect waves grey'>
+								Send
+							</button>
+						</form>
 					</div>
 				</div>
 			</div>
