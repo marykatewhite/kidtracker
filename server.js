aef38fbb85e05cbc83e05ee7c4e392f80d01d018
MaryEtak <marykatewhite@gmail.com>
2020-04-21 10:13:56 -0400

    pushing this us up, but i'mma need to work with a TA on it tonight

--- server.js
+++ server.js
@@ -3,17 +3,40 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = require('http').createServer(app);
-const moment = require ('moment');
+// const moment = require ("moment");
 var io = require('socket.io')(server);
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const UserDetail = require('./models/teacherModel');
 
-var time = moment().format('h:mm a')
+
+// var time = moment().format('h:mm a')
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
 
+// PASSPORT STUFF
+// https://www.sitepoint.com/local-authentication-using-passport-node-js/
+
+// Creating express app and configuring middleware needed for authentication
+/* MONGOOSE SETUP */
+// Finally, we add passportLocalMongoose as a plugin to our Schema. This will work part of the magic we talked about earlier. Then, we create a model from that schema. The first parameter is the name of the collection in the database. The second one is the reference to our Schema, and the third one is the name we’re assigning to the collection inside Mongoose.
+mongoose.connect('mongodb://localhost/kidtracker',
+  { useNewUrlParser: true, useUnifiedTopology: true });
+UserDetail.plugin(passportLocalMongoose);
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+/* PASSPORT LOCAL AUTHENTICATION */
+passport.use(UserDetails.createStrategy());
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
+
+
+
 // Send every request to the React app
+require("./routes/classRoutes.js")(app);
+
 // Define any API routes before this runs
 app.get("*", function (req, res) {
 	res.sendFile(path.join(__dirname, "./client/public/index.html"));
